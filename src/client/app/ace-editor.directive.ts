7bb2bd08d9048e4502dbe40eebe9086bdecd0eff
tfellner-tgm <tfellner@student.tgm.ac.at>
2016-10-15 22:56:32 +0200

    update tslint rules

--- src/client/app/ace-editor.directive.ts
+++ src/client/app/ace-editor.directive.ts
@@ -7,63 +7,64 @@ import 'brace/theme/tomorrow_night';
 declare var ace: any;
 
 @Directive({
-  selector: '[ace-editor]'
+    selector: '[ace-editor]'
 })
 export class AceEditorDirective {
-  public editor: any;
-  public oldVal: any;
+    public editor: any;
+    public oldVal: any;
 
 
-  @Output() public textChanged = new EventEmitter();
-  @Output() public editorRef = new EventEmitter();
+    @Output() public textChanged = new EventEmitter();
+    @Output() public editorRef = new EventEmitter();
 
 
-  private readOnly: any;
-  private theme: any;
-  private mode: any;
-  @Input() set options(value) {
-    this.editor.setOptions(value || {});
-  }
+    private readOnly: any;
+    private theme: any;
+    private mode: any;
 
-  @Input() set readOnly(value) {
-    this.readOnly = value;
-    this.editor.setReadOnly(value);
-  }
+    @Input() set options(value) {
+        this.editor.setOptions(value || {});
+    }
 
-  @Input() set theme(value) {
-    this.theme = value;
-    this.editor.setTheme(`ace/theme/${value}`);
-  }
+    @Input() set readOnly(value) {
+        this.readOnly = value;
+        this.editor.setReadOnly(value);
+    }
 
-  @Input() set mode(value) {
-    this.mode = value;
-    this.editor.getSession().setMode(`ace/mode/${value}`);
-  }
+    @Input() set theme(value) {
+        this.theme = value;
+        this.editor.setTheme(`ace/theme/${value}`);
+    }
 
-  @Input() set text(value) {
-    if(value === this.oldVal) return;
-    this.editor.setValue(value);
-    this.editor.clearSelection();
-    this.editor.focus();
-  }
+    @Input() set mode(value) {
+        this.mode = value;
+        this.editor.getSession().setMode(`ace/mode/${value}`);
+    }
 
-  constructor(private elementRef: ElementRef) {
-    const el = elementRef.nativeElement;
-    el.classList.add('editor');
+    @Input() set text(value) {
+        if(value === this.oldVal) return;
+        this.editor.setValue(value);
+        this.editor.clearSelection();
+        this.editor.focus();
+    }
 
-    this.editor = ace.edit(el);
+    constructor(private elementRef: ElementRef) {
+        const el = elementRef.nativeElement;
+        el.classList.add('editor');
 
-    setTimeout(() => {
-      this.editorRef.next(this.editor);
-    });
+        this.editor = ace.edit(el);
 
-    this.editor.on('change', () => {
-      const newVal = this.editor.getValue();
-      if(newVal === this.oldVal) return;
-      if(typeof this.oldVal !== 'undefined') {
-        this.textChanged.next(newVal);
-      }
-      this.oldVal = newVal;
-    });
-  }
+        setTimeout(() => {
+            this.editorRef.next(this.editor);
+        });
+
+        this.editor.on('change', () => {
+            const newVal = this.editor.getValue();
+            if(newVal === this.oldVal) return;
+            if(typeof this.oldVal !== 'undefined') {
+                this.textChanged.next(newVal);
+            }
+            this.oldVal = newVal;
+        });
+    }
 }
