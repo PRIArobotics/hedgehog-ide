0780e0df613d22b5f725f78f4ee21d53af1c9625
Markus Klein <m@mklein.co.at>
2016-10-17 18:36:21 +0200

    add swagger validate task and travis integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,8 @@
+var fs   = require('fs');
+var path = require('path');
+var spec = require('swagger-tools').specs.v2;
+var yaml = require('js-yaml');
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -52,4 +57,42 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-symlink');
 
     grunt.registerTask('default', ['clean', 'ts', 'copy', 'symlink']);
+
+    grunt.registerTask('swagger-validate', function() {
+        var done = this.async();
+        var specObject = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'api_spec/swagger.yaml'), 'utf8'));
+
+        // from https://github.com/apigee-127/swagger-tools/blob/master/docs/API.md#validaterlorso-apideclarations-callback
+        spec.validate(specObject, function (err, result) {
+            if (err) {
+                done(err);
+            }
+
+            if (typeof result !== 'undefined') {
+                if (result.warnings.length > 0) {
+                    grunt.log.writeln('Warnings');
+                    grunt.log.writeln('--------');
+
+                    result.warnings.forEach(function (warn) {
+                        grunt.log.writeln('#/' + warn.path.join('/') + ': ' + warn.message);
+                    });
+                }
+
+                if (result.errors.length > 0) {
+                    grunt.log.error('Errors');
+                    grunt.log.error('------');
+
+                    result.errors.forEach(function (err) {
+                        grunt.log.error('#/' + err.path.join('/') + ': ' + err.message);
+                    });
+
+                    grunt.fail.fatal('The Swagger document is invalid.');
+                    done(false);
+                    return;
+                }
+            }
+            grunt.log.ok('API specification is valid.');
+            done();
+        });
+    })
 };
