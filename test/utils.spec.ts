646b5851115193974f073352a2de10c618b216bb
Markus Klein <m@mklein.co.at>
2016-11-16 15:42:11 +0100

    move utils test cases to root suite

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -4,38 +4,40 @@ import { basename } from '../src/utils';
 import { dirname }  from '../src/utils';
 import { join }  from '../src/utils';
 
-describe('basename', () => {
-    it('should return path without /\'s', () => {
-        let path = basename('../src/utils.ts');
-        assert.equal('utils.ts', path);
+describe('Utils', () => {
+    describe('basename', () => {
+        it('should return path without /\'s', () => {
+            let path = basename('../src/utils.ts');
+            assert.equal('utils.ts', path);
+        });
+
+        it('should return given path', () => {
+            let path = basename('utils.ts');
+            assert.equal('utils.ts', path);
+        });
     });
 
-    it('should return given path', () => {
-        let path = basename('utils.ts');
-        assert.equal('utils.ts', path);
-    });
-});
+    describe('dirname', () => {
+        it('should return path without filename', () => {
+            let path = dirname('../src/utils.ts');
+            assert.equal('../src', path);
+        });
 
-describe('dirname', () => {
-    it('should return path without filename', () => {
-        let path = dirname('../src/utils.ts');
-        assert.equal('../src', path);
-    });
-
-    it('should return .', () => {
-        let path = dirname('utils.ts');
-        assert.equal('.', path);
-    });
-});
-
-describe('join', () => {
-    it('should return joint paths', () => {
-        let path = join('../src/utils/', '/path/', '/join/');
-        assert.equal('../src/utils/path/join', path);
+        it('should return .', () => {
+            let path = dirname('utils.ts');
+            assert.equal('.', path);
+        });
     });
 
-    it('should return joint paths with / at beginning', () => {
-        let path = join('/src/utils/', '/path/', '/join/');
-        assert.equal('/src/utils/path/join', path);
+    describe('join', () => {
+        it('should return joint paths', () => {
+            let path = join('../src/utils/', '/path/', '/join/');
+            assert.equal('../src/utils/path/join', path);
+        });
+
+        it('should return joint paths with / at beginning', () => {
+            let path = join('/src/utils/', '/path/', '/join/');
+            assert.equal('/src/utils/path/join', path);
+        });
     });
 });
