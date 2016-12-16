2621f94f0b2cb7d24e0838260b9cc78e15435568
Markus Klein <m@mklein.co.at>
2016-12-16 21:32:34 +0100

    keep type entries up to date when files are added/removed as well

--- src/common/versioncontrol/WorkingTreeDirectory.ts
+++ src/common/versioncontrol/WorkingTreeDirectory.ts
@@ -2,7 +2,7 @@
 import WorkingTreeFile from "./WorkingTreeFile";
 import WorkingTreeObject from "./WorkingTreeObject";
 import { WorkingTreeObjectType } from "./WorkingTreeObject";
-import { join } from "../utils";
+import {join, basename} from "../utils";
 
 export default class WorkingTreeDirectory extends WorkingTreeObject {
     public readonly type: WorkingTreeObjectType = WorkingTreeObjectType.Directory;
@@ -45,24 +45,30 @@ export default class WorkingTreeDirectory extends WorkingTreeObject {
         await this.storage.createOrUpdateWorkingTreeFile(this.programName, this.getItemPath(name), content, mode);
 
         // only add the new item if it has not already been done by the program storage
-        if(this.items.indexOf(name) === -1)
+        if(this.items.indexOf(name) === -1) {
             this.items.push(name);
+            this.types[name] = WorkingTreeObjectType.File;
+        }
     }
 
     public async deleteFile(name: string): Promise<void> {
         await this.storage.deleteWorkingTreeObject(this.programName, this.getItemPath(name));
 
         // only delete the item if it has not already been done by the program storage
-        if(this.items.indexOf(name) !== -1)
+        if(this.items.indexOf(name) !== -1) {
             this.items.splice(this.items.indexOf(name));
+            this.types[name] = WorkingTreeObjectType.Directory;
+        }
     }
 
     public async addDirectory(name: string, mode?: number): Promise<void> {
         await this.storage.createWorkingTreeDirectory(this.programName, this.getItemPath(name));
 
         // only add the new item if it has not already been done by the program storage
-        if(this.items.indexOf(name) === -1)
+        if(this.items.indexOf(name) === -1) {
             this.items.push(name);
+            delete this.types[name];
+        }
     }
 
     public deleteDirectory(name: string): Promise<void> {
