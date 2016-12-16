3b54335a2f46c39744a361d707eb2a4993d01629
Markus Klein <m@mklein.co.at>
2016-12-16 23:40:22 +0100

    add documentation for ObjectParser

--- src/server/jsonapi/Parser.ts
+++ src/server/jsonapi/Parser.ts
@@ -1,11 +1,32 @@
+/**
+ * The ObjectParser is used both for validation and parsing JSON (request) data
+ * into the resource model class instances.
+ *
+ * Each parser instance has properties which define the properties on the target model.
+ *
+ * One parser is always responsible for exactly one type of objects which is specified
+ * with the type argument `T`.
+ */
 export class ObjectParser<T> {
     private properties: Map<string, IParserProperty> = new Map();
 
+    /**
+     * Create a ObjectParser
+     *
+     * @param targetFactory Method which is used to create resource model instances
+     * @param properties Rest argument for adding properties directly on the parser creation
+     */
     public constructor(public targetFactory: () => T, ...properties: IParserProperty[]) {
         if(properties)
             this.addProperties.apply(this, properties);
     };
 
+    /**
+     * Parse a single resource
+     *
+     * @param object Deserialized JSON data
+     * @returns {T} Parsed resource
+     */
     public parse(object: Object): T {
         object = object || { };
 
@@ -21,6 +42,12 @@ export class ObjectParser<T> {
         return target;
     }
 
+    /**
+     * Parse an array of same-typed resources at once
+     *
+     * @param objects array of JSON objects
+     * @returns {T[]} array containing the parsed resources
+     */
     public parseArray(objects: Object[]): T[] {
         let parsedObjects = [];
         for(const object of objects) {
@@ -29,6 +56,12 @@ export class ObjectParser<T> {
         return parsedObjects;
     }
 
+    /**
+     * Add one or more properties to the parser.
+     * Existing properties will be overwritten.
+     *
+     * @param properties Rest argument; properties to add (see {@link IParserProperty})
+     */
     public addProperties(...properties: IParserProperty[]) {
         for(const property of properties) {
             if(!property.handler)
