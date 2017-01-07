6b5882b0caab70613b0c4f806544a9d337738b4d
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-01-07 23:29:54 +0100

    renamed file; using AfterViewInit nocw

--- src/client/app/blockly/editor.component.ts
+++ src/client/app/blockly/editor.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 declare var Blockly: any;
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/blockly/blockly.component.html'
+  templateUrl: 'app/blockly/editor.component.html'
 })
-export class BlocklyComponent implements OnInit {
+export class BlocklyComponent implements AfterViewInit {
 
     private _workspace: any;
 
-    ngOnInit(): void {
+    ngAfterViewInit(): void {
         var workspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox')});
 
         this._workspace = Blockly.inject('blocklyDiv');
