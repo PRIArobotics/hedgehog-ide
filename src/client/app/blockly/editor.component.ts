45685f990da3a5bfc9a39e8b696e3cbcf795be2b
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-01-08 20:07:52 +0100

    moved toolbox xml out of the view because TS keeps complaining about the non-standard DOM elements; this is a really messy workaround

--- src/client/app/blockly/editor.component.ts
+++ src/client/app/blockly/editor.component.ts
@@ -12,19 +12,32 @@ export class BlocklyComponent implements AfterViewInit {
 
     ngAfterViewInit(): void {
 
-        let toolbox: any = {toolbox: document.getElementById('toolbox'),
+
+        let s: string = `<xml id="toolbox" style="display: none">
+            <block type="controls_if"></block>
+        </xml>`;
+
+        var div: any = document.createElement('div');
+        div.innerHTML = s;
+        var elements: any = div.firstChild;
+
+        let toolbox: any = {toolbox: elements,
             zoom: {controls: true,
                    wheel: true,
                    startScale: 1.0,
                    maxScale: 2,
                    minScale: 0.8,
-                   scaleSpeed: 1.1},
+                   scaleSpeed: 1.05},
             grid: {spacing: 20,
                    length: 3,
                    colour: '#ccc',
                    snap: true},
-            trashcan: true
+            trashcan: true,
+            maxBlocks: 100,
+            media: 'app/blockly/lib/media/'
         };
+
+        console.log(toolbox);
 
         this._workspace = Blockly.inject('blocklyDiv', toolbox);
 
