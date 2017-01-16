59cb2769d69c829bae05b819719f8322f7a648a0
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-01-16 15:03:52 +0100

    add local storage; methods for workspace conversion; move toolbox xml to extra method;

--- src/client/app/blockly/editor.component.ts
+++ src/client/app/blockly/editor.component.ts
@@ -1,35 +1,56 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
+import {LocalStorageService} from "angular2-localstorage";
+import {LocalStorage} from "angular2-localstorage";
+
+/*
+ * TODO: add toast notifications, http storage
+ */
 declare var Blockly: any;
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/blockly/editor.component.html'
+    templateUrl: 'app/blockly/editor.component.html',
+    providers: [
+        LocalStorageService,
+    ]
 })
-export class BlocklyComponent implements AfterViewInit {
+export class BlocklyComponent implements AfterViewInit,OnInit {
 
-    private _workspace: any;
+    private workspace: any;
 
     // The Program name that is passed to this class by the router
     private programName: string;
 
+    // local storage Object as programName: { }
+    @LocalStorage()
+    private localStorageFiles: {[programName: string]: {[fileId: string]: string}};
+
     constructor(route: ActivatedRoute) {
         this.programName = route.snapshot.params['programName'];
+        this.localStorageFiles = {};
     }
 
     public clearWorkspace(): void {
-        this._workspace.clear();
+        this.workspace.clear();
+    }
+
+    public saveWorkspace(): void {
+        this.localStorageFiles[this.programName]["xml"] = this.toXML();
+    }
+
+    public async ngOnInit() {
+        // check if the local storage already has this program stored
+        if (!this.localStorageFiles[this.programName]) {
+            // if not create a new Map with the program name
+            this.localStorageFiles[this.programName] = {};
+        }
     }
 
     public ngAfterViewInit(): void {
 
-
-        let tools: string = `<xml id="toolbox" style="display: none">
-            <block type="controls_if"></block>
-        </xml>`;
-
-        let toolbox: any = {toolbox: tools,
+        let toolbox: any = {toolbox: this.getToolbox(),
             zoom: {controls: true,
                    wheel: true,
                    startScale: 1.0,
@@ -45,10 +66,327 @@ export class BlocklyComponent implements AfterViewInit {
             media: 'app/blockly/lib/media/'
         };
 
-        console.log(toolbox);
-
-        this._workspace = Blockly.inject('blocklyDiv', toolbox);
+        this.workspace = Blockly.inject('blocklyDiv', toolbox);
+        this.toWorkspace(this.localStorageFiles[this.programName]["xml"]);
+        this.workspace.addChangeListener(e => this.saveWorkspace());
 
     }
 
+    private toWorkspace(xml: string) {
+        let dom = Blockly.Xml.textToDom(xml);
+        Blockly.Xml.domToWorkspace(dom, this.workspace);
+    }
+
+    private toPython(): string {
+        Blockly.Python.INFINITE_LOOP_TRAP = null;
+        return Blockly.Python.workspaceToCode(this.workspace);
+    }
+
+    private toXML(): string {
+        let dom = Blockly.Xml.workspaceToDom(this.workspace);
+        return Blockly.Xml.domToText(dom);
+    }
+
+    private getToolbox(): string {
+        return `<xml id="toolbox" style="display: none">
+      <category name="Hedgehog" colour="120">
+        <block type="hedgehog_move"></block>
+        <block type="hedgehog_read_analog"></block>
+        <block type="hedgehog_read_digital"></block>
+      </category>
+      <category name="Logic" colour="210">
+        <block type="controls_if"></block>
+        <block type="logic_compare"></block>
+        <block type="logic_operation"></block>
+        <block type="logic_negate"></block>
+        <block type="logic_boolean"></block>
+        <block type="logic_null"></block>
+        <block type="logic_ternary"></block>
+      </category>
+      <category name="Loops" colour="60">
+        <block type="controls_repeat_ext">
+          <value name="TIMES">
+            <shadow type="math_number">
+              <field name="NUM">10</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="controls_whileUntil"></block>
+        <block type="controls_for">
+          <value name="FROM">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+          <value name="TO">
+            <shadow type="math_number">
+              <field name="NUM">10</field>
+            </shadow>
+          </value>
+          <value name="BY">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="controls_forEach"></block>
+        <block type="controls_flow_statements"></block>
+      </category>
+      <category name="Math" colour="230">
+        <block type="math_number"></block>
+        <block type="math_arithmetic">
+          <value name="A">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+          <value name="B">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_single">
+          <value name="NUM">
+            <shadow type="math_number">
+              <field name="NUM">9</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_trig">
+          <value name="NUM">
+            <shadow type="math_number">
+              <field name="NUM">45</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_constant"></block>
+        <block type="math_number_property">
+          <value name="NUMBER_TO_CHECK">
+            <shadow type="math_number">
+              <field name="NUM">0</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_round">
+          <value name="NUM">
+            <shadow type="math_number">
+              <field name="NUM">3.1</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_on_list"></block>
+        <block type="math_modulo">
+          <value name="DIVIDEND">
+            <shadow type="math_number">
+              <field name="NUM">64</field>
+            </shadow>
+          </value>
+          <value name="DIVISOR">
+            <shadow type="math_number">
+              <field name="NUM">10</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_constrain">
+          <value name="VALUE">
+            <shadow type="math_number">
+              <field name="NUM">50</field>
+            </shadow>
+          </value>
+          <value name="LOW">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+          <value name="HIGH">
+            <shadow type="math_number">
+              <field name="NUM">100</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_random_int">
+          <value name="FROM">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+          <value name="TO">
+            <shadow type="math_number">
+              <field name="NUM">100</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="math_random_float"></block>
+      </category>
+      <category name="Text" colour="160">
+        <block type="text"></block>
+        <block type="text_join"></block>
+        <block type="text_append">
+          <value name="TEXT">
+            <shadow type="text"></shadow>
+          </value>
+        </block>
+        <block type="text_length">
+          <value name="VALUE">
+            <shadow type="text">
+              <field name="TEXT">abc</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="text_isEmpty">
+          <value name="VALUE">
+            <shadow type="text">
+              <field name="TEXT"></field>
+            </shadow>
+          </value>
+        </block>
+        <block type="text_indexOf">
+          <value name="VALUE">
+            <block type="variables_get">
+              <field name="VAR">list</field>
+            </block>
+          </value>
+          <value name="FIND">
+            <shadow type="text">
+              <field name="TEXT">abc</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="text_charAt">
+          <value name="VALUE">
+            <block type="variables_get">
+              <field name="VAR">list</field>
+            </block>
+          </value>
+        </block>
+        <block type="text_getSubstring">
+          <value name="STRING">
+            <block type="variables_get">
+              <field name="VAR">list</field>
+            </block>
+          </value>
+        </block>
+        <block type="text_changeCase">
+          <value name="TEXT">
+            <shadow type="text">
+              <field name="TEXT">abc</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="text_trim">
+          <value name="TEXT">
+            <shadow type="text">
+              <field name="TEXT">abc</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="text_print">
+          <value name="TEXT">
+            <shadow type="text">
+              <field name="TEXT">abc</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="text_prompt_ext">
+          <value name="TEXT">
+            <shadow type="text">
+              <field name="TEXT">abc</field>
+            </shadow>
+          </value>
+        </block>
+      </category>
+      <category name="Lists" colour="260">
+        <block type="lists_create_with">
+          <mutation items="0"></mutation>
+        </block>
+        <block type="lists_create_with"></block>
+        <block type="lists_repeat">
+          <value name="NUM">
+            <shadow type="math_number">
+              <field name="NUM">5</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="lists_length"></block>
+        <block type="lists_isEmpty"></block>
+        <block type="lists_indexOf">
+          <value name="VALUE">
+            <block type="variables_get">
+              <field name="VAR">list</field>
+            </block>
+          </value>
+        </block>
+        <block type="lists_getIndex">
+          <value name="VALUE">
+            <block type="variables_get">
+              <field name="VAR">list</field>
+            </block>
+          </value>
+        </block>
+        <block type="lists_setIndex">
+          <value name="LIST">
+            <block type="variables_get">
+              <field name="VAR">list</field>
+            </block>
+          </value>
+        </block>
+        <block type="lists_getSublist">
+          <value name="LIST">
+            <block type="variables_get">
+              <field name="VAR">list</field>
+            </block>
+          </value>
+        </block>
+        <block type="lists_split">
+          <value name="DELIM">
+            <shadow type="text">
+              <field name="TEXT">,</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="lists_sort"></block>
+      </category>
+      <category name="Colors" colour="20">
+        <block type="colour_picker"></block>
+        <block type="colour_random"></block>
+        <block type="colour_rgb">
+          <value name="RED">
+            <shadow type="math_number">
+              <field name="NUM">100</field>
+            </shadow>
+          </value>
+          <value name="GREEN">
+            <shadow type="math_number">
+              <field name="NUM">50</field>
+            </shadow>
+          </value>
+          <value name="BLUE">
+            <shadow type="math_number">
+              <field name="NUM">0</field>
+            </shadow>
+          </value>
+        </block>
+        <block type="colour_blend">
+          <value name="COLOUR1">
+            <shadow type="colour_picker">
+              <field name="COLOUR">#ff0000</field>
+            </shadow>
+          </value>
+          <value name="COLOUR2">
+            <shadow type="colour_picker">
+              <field name="COLOUR">#3333ff</field>
+            </shadow>
+          </value>
+          <value name="RATIO">
+            <shadow type="math_number">
+              <field name="NUM">0.5</field>
+            </shadow>
+          </value>
+        </block>
+      </category>
+      <sep></sep>
+      <category name="Variables" colour="330" custom="VARIABLE"></category>
+      <category name="Functions" colour="290" custom="PROCEDURE"></category>`;
+    }
 }
