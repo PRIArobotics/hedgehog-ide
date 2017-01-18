5aaa6c04643e219245156b7aad0ee14dd371953b
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-01-18 16:28:49 +0100

    add MaterializeModule

--- src/client/app/blockly/editor.module.ts
+++ src/client/app/blockly/editor.module.ts
@@ -1,7 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA }  from '@angular/core';
 import { BlocklyComponent }  from './editor.component';
+import {MaterializeModule} from "angular2-materialize";
 
 @NgModule({
+    imports: [MaterializeModule],
     declarations: [BlocklyComponent],
     exports: [BlocklyComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
