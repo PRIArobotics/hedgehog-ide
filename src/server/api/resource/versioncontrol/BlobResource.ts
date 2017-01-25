ad0953f336453124908d82cce1e54548a8e0909d
Markus Klein <m@mklein.co.at>
2017-01-25 13:13:43 +0100

    fix self links

--- src/server/api/resource/versioncontrol/BlobResource.ts
+++ src/server/api/resource/versioncontrol/BlobResource.ts
@@ -31,7 +31,7 @@ export default class BlobResource extends ApiResource {
         }
 
         let documentBuilder = new JsonApiDocumentBuilder();
-        documentBuilder.setLinks(getLinkUrl(req, `/api/programs/${genericToBase64(programName)}/${blobId}`), null);
+        documentBuilder.setLinks(getLinkUrl(req, `/api/blobs/${genericToBase64(programName)}/${blobId}`), null);
         documentBuilder.addResource( await this.serializerRegistry.serialize(blob, req, documentBuilder));
 
         return reply(documentBuilder.getProduct())
--- src/server/api/resource/versioncontrol/TreeResource.ts
+++ src/server/api/resource/versioncontrol/TreeResource.ts
@@ -31,7 +31,7 @@ export default class TreeResource extends ApiResource {
         }
 
         let documentBuilder = new JsonApiDocumentBuilder();
-        documentBuilder.setLinks(getLinkUrl(req, `/api/programs/${genericToBase64(programName)}/${treeId}`), null);
+        documentBuilder.setLinks(getLinkUrl(req, `/api/trees/${genericToBase64(programName)}/${treeId}`), null);
         documentBuilder.addResource( await this.serializerRegistry.serialize(tree, req, documentBuilder));
 
         return reply(documentBuilder.getProduct())
