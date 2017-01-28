b0af53406d74f6ea0c3e5408702b8707fe90af9b
Markus Klein <m@mklein.co.at>
2017-01-28 20:55:29 +0100

    simplify systemjs config and load all scripts via systemjs

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,9 +1,10 @@
+import './vendor';
 import "materialize-css";
 import "angular2-materialize";
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
--- src/client/systemjs.config.js
+++ src/client/systemjs.config.js
@@ -5,94 +5,32 @@
 (function (global) {
     System.config({
         defaultJSExtensions: true,
+        packageConfigPaths: [
+            './node_modules/*/package.json',
+            './node_modules/@angular/*/package.json'
+        ],
         paths: {
-            // paths serve as alias
-            'npm:': 'node_modules/',
-
-            'brace': 'npm:brace@0.8.0',
-            'w3c-blob': 'npm:w3c-blob/index.js',
-            'buffer': 'npm:buffer/index.js',
-            'base64-js': 'npm:base64-js/index.js',
-            'ieee754': 'npm:ieee754/index.js',
-
-            rxjs: 'node_modules/rxjs',
-            'materialize-css': 'node_modules/materialize-css',
-            'angular2-materialize': 'node_modules/angular2-materialize',
-            'angular2-contextmenu': 'node_modules/angular2-contextmenu',
-            'angular2-localstorage': 'node_modules/angular2-localstorage'
+            '*': './node_modules/*',
+            'app': 'app/',
+            'app/*': 'app/*',
+            'vendor': 'vendor',
+            'main': 'main',
         },
-        // map tells the System loader where to look for things
         map: {
-            // our app is within the app folder
-            app: 'app',
-            // angular bundles
-            '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-            '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-            '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-            // other libraries
-            'rxjs':                      'npm:rxjs',
-            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
-
-            'brace': 'npm:brace',
-            'w3c-blob': 'npm:w3c-blob/index.js',
-            'buffer': 'npm:buffer/index.js',
-            'base64-js': 'npm:base64-js/index.js',
-            'ieee754': 'npm:ieee754/index.js',
-
-            'angular2-tree-component': 'npm:angular2-tree-component',
-            'lodash': 'npm:lodash',
-
-            'socket.io-client': 'npm:socket.io-client/dist/socket.io.min.js',
-            'utf8': 'npm:utf8/utf8.js'
+            'socket.io-client': './node_modules/socket.io-client/dist/socket.io.min.js',
+            'materialize-css': './node_modules/materialize-css/dist/js/materialize',
         },
-        // packages tells the System loader how to load when no filename and/or no extension
-        packages: {
-            app: {
-                main: './main.js',
-                defaultExtension: 'js'
+        meta: {
+            'materialize-css': { format: 'global' },
+            'babel-polyfill/dist/polyfill': { format: 'global' },
+            'zone.js/dist/zone': { format: 'global' },
+            'reflect-metadata/Reflect': { format: 'global' },
+            'jquery': { format: 'global' },
+            'jquery-ui-dist/jquery-ui': { format: 'global' },
+            'ace-builds/src-noconflict/ace': {
+                format: 'global'
             },
-            rxjs: {
-                main: './Rx.js',
-                defaultExtension: 'js'
-            },
-            'angular-in-memory-web-api': {
-                main: './index.js',
-                defaultExtension: 'js'
-            },
-            brace: {
-                main: './index.js',
-                defaultExtension: 'js'
-            },
-            'materialize-css': {
-                format: "global",
-                main: "./dist/js/materialize",
-                defaultExtension: "js"
-            },
-            'angular2-materialize': {
-                main: "dist/index",
-                defaultExtension: "js"
-            },
-            'angular2-tree-component': {
-                main: './dist/angular2-tree-component.js',
-                defaultExtension: 'js'
-            },
-            'lodash': {
-                main: './lodash',
-                defaultExtension: 'js'
-            },
-            'angular2-contextmenu': {
-                main: './angular2-contextmenu',
-                defaultExtension: 'js'
-            },
-            'angular2-localstorage': {
-                main: './dist/index',
-                defaultExtension: 'js'
-            }
+            'ace-builds/src-noconflict/ext-language_tools': { format: 'global', deps: ['ace-builds/src-noconflict/ace'] }
         }
     });
 })(this);
