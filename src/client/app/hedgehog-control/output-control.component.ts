fe57c3dd04b25bdde6d608b76bb2e453e8196945
Markus Klein <m@mklein.co.at>
2017-02-11 18:56:19 +0100

    add inputs and outputs to output control component

--- src/client/app/hedgehog-control/output-control.component.ts
+++ src/client/app/hedgehog-control/output-control.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
     selector: 'output-control',
     template: require('./output-control.component.html'),
     styles: [require('./output-control.component.css')]
 })
-export default class OutputControlComponent implements OnInit {
-    public value: number = 0;
+export default class OutputControlComponent {
+    @Input() public value: number;
+    @Input() public state: boolean;
 
-    constructor() { }
+    @Output() private stateChanged = new EventEmitter();
+    @Output() private valueChanged = new EventEmitter();
 
-    public ngOnInit() { }
+    private updateValue (value) {
+        this.value = value;
+        this.valueChanged.emit(value);
+    }
 
+    private updateState (state) {
+        this.state = state;
+        this.valueChanged.emit(state);
+    }
 }
