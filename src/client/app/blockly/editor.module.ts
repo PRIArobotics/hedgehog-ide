ba97762c403e39991a95df296b84a0dfae236fc1
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-02-22 23:44:34 +0100

    add TextIdeModule to use the Ace editor

--- src/client/app/blockly/editor.module.ts
+++ src/client/app/blockly/editor.module.ts
@@ -1,9 +1,10 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { BlocklyComponent } from './editor.component';
+import {BlocklyComponent} from './editor.component';
 import {MaterializeModule} from "angular2-materialize";
 import {ProgramExecutionModule} from '../program-execution/program-execution.module';
 import {AngularSplitModule} from "angular-split";
+import {TextIdeModule} from "../text-ide/text-ide.module";
 
 @NgModule({
     imports: [
@@ -11,9 +12,14 @@ import {AngularSplitModule} from "angular-split";
         CommonModule,
         ProgramExecutionModule,
         AngularSplitModule,
+        TextIdeModule,
     ],
-    declarations: [BlocklyComponent],
-    exports: [BlocklyComponent],
+    declarations: [
+        BlocklyComponent,
+    ],
+    exports: [
+        BlocklyComponent,
+    ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BlocklyModule { }
