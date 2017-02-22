1db6ca67f6e136945c8ace89c2accdba82e7cdf9
Markus Klein <m@mklein.co.at>
2017-02-22 08:59:55 +0100

    implement servo resource

---
+++ src/server/api/resource/hedgehog-io/ServoResource.ts
@@ -0,0 +1,54 @@
+import Hapi = require('hapi');
+import winston = require("winston");
+import {HedgehogClient} from 'hedgehog-client';
+
+import ApiResource from "../../ApiResource";
+import SerializerRegistry from "../../../serializer/SerializerRegistry";
+import {IReply} from "hapi";
+import {JsonApiDocument, JsonApiResource} from "../../../jsonapi/JsonApiObjects";
+import {RequirementType, ObjectParser} from "../../../jsonapi/Parser";
+import ApiEndpoint from "../../ApiEndpoint";
+
+export default class ServoResource extends ApiResource {
+    public constructor (private hedgehog: HedgehogClient, private serializerRegistry: SerializerRegistry) {
+        super('/servos');
+    }
+
+    @ApiEndpoint('PATCH', '/{servoId}')
+    public setServo (req: Hapi.Request, reply: IReply) {
+        const servoPort = Number(req.params['servoId']);
+
+        let parser = JsonApiDocument.getParser().addProperties({
+            name: 'data',
+            required: RequirementType.Required,
+            handler: JsonApiResource.getParser().addProperties({
+                name: 'attributes',
+                required: RequirementType.Required,
+                handler: new ObjectParser(() => ({}),
+                    {
+                        name: 'enabled',
+                        required: RequirementType.Required
+                    },
+                    {
+                        name: 'position',
+                        required: RequirementType.Required
+                    }
+                )
+            })
+        });
+
+        let requestData: JsonApiResource;
+        try {
+            requestData = parser.parse(req.payload).data as JsonApiResource;
+        } catch (err) {
+            winston.error(err);
+            return reply({
+                error: 'Error while parsing the request. Argument might be missing.'
+            }).code(400);
+        }
+
+
+        this.hedgehog.setServo(servoPort, requestData.attributes.enabled, Number(requestData.attributes.position));
+        return reply('');
+    }
+}
