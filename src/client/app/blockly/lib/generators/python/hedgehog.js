0f94d05640e97e7a4b999cd323b72f90c3c4e2ed
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-02-22 13:51:09 +0100

    add hedgehog code generators

---
+++ src/client/app/blockly/lib/generators/python/hedgehog.js
@@ -0,0 +1,62 @@
+'use strict';
+
+goog.provide('Blockly.Python.hedgehog');
+
+goog.require('Blockly.Python');
+
+Blockly.Python['hedgehog_scope'] = function(block) {
+    var statements = Blockly.Python.statementToCode(block, 'IN');
+    Blockly.Python.definitions_['import_hedgehog'] = 'from hedgehog.client import connect';
+
+    var code = 'with connect(emergency=15) as hedgehog:\n' + statements;
+    return code;
+};
+
+Blockly.Python['hedgehog_turn'] = function(block) {
+    var number_motor1 = block.getFieldValue('MOTOR1');
+    var number_motor2 = block.getFieldValue('MOTOR2');
+    var dropdown_dir = block.getFieldValue('DIR');
+    var value_num = Blockly.Python.valueToCode(block, 'NUM', Blockly.Python.ORDER_ATOMIC);
+    var dropdown_unit = block.getFieldValue('UNIT');
+
+    // imports
+    Blockly.Python.definitions_['import_sleep'] = 'from time import sleep';
+    Blockly.Python.definitions_['import_hedgehog'] = 'from hedgehog.client import connect';
+
+    // TODO: Assemble Python into code variable.
+    var code = 'blub\n';
+    return code;
+};
+
+Blockly.Python['hedgehog_move'] = function(block) {
+    var port = block.getFieldValue('PORT');
+    var speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
+    var time = Blockly.Python.valueToCode(block, 'TIME', Blockly.Python.ORDER_ATOMIC);
+
+    // imports
+    Blockly.Python.definitions_['import_sleep'] = 'from time import sleep';
+    Blockly.Python.definitions_['import_hedgehog'] = 'from hedgehog.client import connect';
+
+    var code = 'hedgehog.move(' + port + ', ' + speed + ')' + '; sleep(' + time + ')\n';
+    // return [code, Blockly.Python.ORDER_FUNCTION];
+    return code;
+};
+
+Blockly.Python['hedgehog_speed'] = function(block) {
+    var speed = block.getFieldValue('SPEED');
+    return [speed, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['hedgehog_read_analog'] = function(block) {
+    var port = block.getFieldValue('PORT');
+
+    var code = 'get_analog(' + port + ')';
+    return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['hedgehog_read_digital'] = function(block) {
+    var port = block.getFieldValue('PORT');
+
+    var code = 'get_digital(' + port + ')';
+    return [code, Blockly.Python.ORDER_NONE];
+};
