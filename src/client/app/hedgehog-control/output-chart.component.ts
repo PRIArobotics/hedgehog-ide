5a8f9b2b7d5eb3c04a1ca5c3cbe889da979927cc
Thomas Fellner <tfellner@student.tgm.ac.at>
2017-02-22 09:17:03 +0100

    fix bug where only 15 sensors were given instead of 16

---
+++ src/client/app/hedgehog-control/output-chart.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+    selector: 'output-control',
+    template: require('./output-control.component.html'),
+    styles: [require('./output-control.component.css')]
+})
+export default class OutputControlComponent {
+    @Input() public name: string;
+    @Input() public value: number;
+    @Input() public state: boolean;
+
+    @Output() private stateChanged = new EventEmitter();
+    @Output() private valueChanged = new EventEmitter();
+
+    private updateValue (value) {
+        this.value = value;
+        this.valueChanged.emit(value);
+    }
+
+    private updateState (state) {
+        this.state = state;
+        this.stateChanged.emit(state);
+    }
+}
