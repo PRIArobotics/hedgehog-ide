2c2b1e87e2ec62b8a05cc6ef8392747615803a84
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-02-22 22:42:26 +0100

    add AngularSplit module

--- src/client/app/blockly/editor.module.ts
+++ src/client/app/blockly/editor.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import { BlocklyComponent } from './editor.component';
 import {MaterializeModule} from "angular2-materialize";
 import {ProgramExecutionModule} from '../program-execution/program-execution.module';
+import {AngularSplitModule} from "angular-split";
 
 @NgModule({
     imports: [
         MaterializeModule,
         CommonModule,
         ProgramExecutionModule,
+        AngularSplitModule,
     ],
     declarations: [BlocklyComponent],
     exports: [BlocklyComponent],
