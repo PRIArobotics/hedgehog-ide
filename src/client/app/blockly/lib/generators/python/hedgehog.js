b4b7816e4cdc61a5840065d324c70492c0937397
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-03-09 23:31:27 +0100

    directly convert angle to 0-2048

--- src/client/app/blockly/lib/generators/python/hedgehog.js
+++ src/client/app/blockly/lib/generators/python/hedgehog.js
@@ -79,7 +79,7 @@ Blockly.Python['hedgehog_servo'] = function(block) {
     Blockly.Python.definitions_['import_sleep'] = 'from time import sleep';
     Blockly.Python.definitions_['import_hedgehog'] = 'from hedgehog.client import connect';
 
-    var code = 'hedgehog.set_servo(' + port + ', True, ' + angle + '*22' + ')\n';
+    var code = 'hedgehog.set_servo(' + port + ', True, ' + Math.floor(parseInt(angle)*11.37) + ')\n';
     code += 'sleep(0.1)\n\n';
     return code;
 };
