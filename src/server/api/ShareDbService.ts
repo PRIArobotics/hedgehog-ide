182a754d323e776d762ed70f6bba7550bf574259
Thomas Fellner <tfellner@student.tgm.ac.at>
2017-03-18 12:15:50 +0100

    add hardcoded server implementation

--- src/server/api/ShareDbService.ts
+++ src/server/api/ShareDbService.ts
@@ -7,6 +7,29 @@ export default class ShareDbService {
 
     public constructor (server) {
         this.backend = new ShareDB();
+        this.backend.connect();
+
+        let connection = this.backend.connect();
+
+        let doc = connection.get('hedgehog-ide', 'program');
+        doc.create({
+                    'Li90ZXN0LnB5': `import time
+import sys
+
+for i in range(100):
+    print(i)
+    sys.stdout.flush()
+    
+time.sleep(5)`,
+                    'YXNkZmdoL2FnYXNkZmcucHk_': `from time import sleep
+from hedgehog.client import connect
+
+with connect(emergency=15) as hedgehog:
+	print("Hello World")
+
+`,
+                    'YXNkZmdoL2RmYXNnZGY_': ``
+        });
 
         let wss = new WebSocket.Server({server});
         wss.on('connection', ws => {
