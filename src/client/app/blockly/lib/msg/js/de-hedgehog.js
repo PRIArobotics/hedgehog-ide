903fac793cc533bf48fda0bb70c91597d455a111
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-04-14 02:22:01 +0200

    add localization for english and german

---
+++ src/client/app/blockly/lib/msg/js/de-hedgehog.js
@@ -0,0 +1,37 @@
+// This file was automatically generated.  Do not modify.
+
+'use strict';
+
+goog.provide('Blockly.Msg.de.hedgehog');
+
+goog.require('Blockly.Msg');
+
+Blockly.Msg.HEDGEHOG_SCOPE = "Hedgehog scope";
+Blockly.Msg.HEDGEHOG_WARN = "Dieser Block muss sich innerhalb eines Hedgehog Scope Blocks befinden!";
+Blockly.Msg.HEDGEHOG_SCOPE_TOOLTIP = "Alle Hedgehog Blöcke müssen sich innerhalb eines Hedgehog scope Blocks befinden";
+
+Blockly.Msg.HEDGEHOG_READ_ANALOG = "analoger Port %1";
+Blockly.Msg.HEDGEHOG_READ_ANALOG_TOOLTIP = "gibt den Wert eines analogen Ports zurück";
+
+Blockly.Msg.HEDGEHOG_READ_DIGITAL = "digitaler Port %1";
+Blockly.Msg.HEDGEHOG_READ_DIGITAL_TOOLTIP = "gibt den Wert eines analogen Ports zurück";
+
+Blockly.Msg.HEDGEHOG_SLEEP = "pausiere für %1 Sekunden";
+Blockly.Msg.HEDGEHOG_SLEEP_TOOLTIP = "pausiert den Programmablauf für eine spezifische Zeit";
+
+Blockly.Msg.HEDGEHOG_SERVO = "setze Servo %1 auf %2 Grad";
+Blockly.Msg.HEDGEHOG_SERVO_TOOLTIP = "verändert die position eines Servos";
+
+Blockly.Msg.HEDGEHOG_FORWARD = "vorwärts";
+Blockly.Msg.HEDGEHOG_BACKWARD = "rückwärts";
+Blockly.Msg.HEDGEHOG_RIGHT = "rechts ⟳";
+Blockly.Msg.HEDGEHOG_LEFT = "links ⟲";
+
+Blockly.Msg.HEDGEHOG_TURN = "wende mit Motor %1 und %2 %3 für %4 Sekunden";
+Blockly.Msg.HEDGEHOG_TURN_TOOLTIP = "wendet den Hedgehog indem zwei Motoren in entgegengesetzter Richtung laufen gelassen werden";
+
+Blockly.Msg.HEDGEHOG_MOVE = "bewege Motor %1 %2 für %3 Sekunden";
+Blockly.Msg.HEDGEHOG_MOVE_TOOLTIP = "Bewegt einen Motor in eine bestimmte Richtung";
+
+Blockly.Msg.HEDGEHOG_MOVE2 = "bewege Motor %1 und %2 %3 für %4 Sekunden";
+Blockly.Msg.HEDGEHOG_MOVE2_TOOLTIP = "Bewegt zwei Motoren in eine bestimmte Richtung";
