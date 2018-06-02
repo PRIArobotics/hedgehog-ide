b421398126ac14d0682697842126999cb7553e0b
SillyFreak <sillyfreak.ck@gmail.com>
2018-06-02 12:35:35 +0200

    make the model easier to write by replacing lists whose items have name/key properties with dicts that use these names/keys. The model is augmented when read to be easy to work with (same in-memory model as before)

--- gsl_blockly/__init__.py
+++ gsl_blockly/__init__.py
@@ -6,11 +6,36 @@ def get_model(model_file, msg_model_file):
         yaml = YAML(typ='safe')
         model = yaml.load(f)
         msg_model = yaml.load(msg_f)
-        msg_modules = {module.name: module.langs for module in msg_model.modules}
-        for module in model.modules:
-            if module.name in msg_modules:
-                module.langs = msg_modules[module.name]
-        return model
+
+    def augment_module(name, mod):
+        def augment_block(name, block):
+            block.name = name
+            return block
+
+        def augment_lang(key, lang):
+            def augment_block(name, block):
+                block.name = name
+                return block
+
+            blocks = [augment_block(*pair) for pair in lang.items()]
+            lang.clear()
+            lang.key = key
+            lang.blocks = blocks
+
+            return lang
+
+        blocks = [augment_block(*pair) for pair in mod.items()]
+        mod.clear()
+        mod.name = name
+        mod.blocks = blocks
+        if name in msg_model.modules:
+            mod.langs = [augment_lang(*pair) for pair in msg_model.modules[name].items()]
+
+        return mod
+
+    model.modules = [augment_module(*pair) for pair in model.modules.items()]
+
+    return model
 
 
 def main():
