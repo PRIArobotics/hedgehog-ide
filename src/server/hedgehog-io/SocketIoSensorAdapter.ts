99de3faf73a7a0240292a378fc0a78ad643fb8f4
SillyFreak <sillyfreak.ck@gmail.com>
2018-11-08 17:55:50 +0100

    use multipart messages for sensor updates to reduce number of requests by a factor of 16. This update changes error behavior: until now, an error requesting one of the sensors would result in an error and a completely missed update (the client does not yet handle negative acknowledgement, but returns null messages; the getAnalog etc. wrappers expect non-null messages). Now, an invalid update results in a missed update for the single sensor.

--- src/server/hedgehog-io/SocketIoSensorAdapter.ts
+++ src/server/hedgehog-io/SocketIoSensorAdapter.ts
@@ -1,28 +1,36 @@
-import {HedgehogClient} from 'hedgehog-client';
+import { HedgehogClient, Message, analog, digital } from 'hedgehog-client';
 
 export default class SocketIoSensorAdapter {
     private ns: SocketIO.Namespace;
+    private requests: Message[] = [];
 
     public constructor (private hedgehog: HedgehogClient, io: SocketIO.Server) {
         this.ns = io.of('/sensors');
+        for (let port = 0; port < 16; port++) {
+            this.requests.push(new analog.Request(port), new digital.Request(port));
+        }
         setInterval(async () => this.sendSensorUpdate(), 200);
     }
 
     private async sendSensorUpdate () {
         let sensorData: Array<{id: number, value: number, type: string}> = [];
-        for (let port = 0; port < 16; port++) {
-            sensorData.push({
-                id: port,
-                type: 'analog',
-                value: await this.hedgehog.getAnalog(port)
-            });
-            sensorData.push({
-                id: port,
-                type: 'digital',
-                value: await this.hedgehog.getDigital(port)? 1 : 0
-            });
+        for (let reply of await this.hedgehog.sendMultipart(...this.requests)) {
+            let id: number;
+            let type: string;
+            let value: number;
+            if (reply instanceof analog.Reply) {
+                id = reply.port;
+                type = 'analog';
+                value = reply.value;
+            } else if (reply instanceof digital.Reply) {
+                id = reply.port;
+                type = 'digital';
+                value = reply.value? 1 : 0;
+            } else {
+                continue;
+            }
+            sensorData.push({ id, type, value });
         }
-
         this.ns.emit('data', sensorData);
     }
 }
