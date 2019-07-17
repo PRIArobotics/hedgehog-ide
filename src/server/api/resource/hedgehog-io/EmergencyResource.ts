a151892f7acca67f05d13e3f3a79023534d77b5d
SillyFreak <sillyfreak.ck@gmail.com>
2019-07-17 15:45:51 +0200

    add API endpoint for setting the emergency state

--- src/server/api/resource/hedgehog-io/EmergencyResource.ts
+++ src/server/api/resource/hedgehog-io/EmergencyResource.ts
@@ -1,4 +1,5 @@
-import {Request} from "hapi";
+import winston = require("winston");
+import {Request, ResponseToolkit} from "hapi";
 import {HedgehogClient} from 'hedgehog-client';
 
 import ApiResource from "../../ApiResource";
@@ -6,7 +7,9 @@ import SerializerRegistry from "../../../serializer/SerializerRegistry";
 import ApiEndpoint from "../../ApiEndpoint";
 import {getLinkUrl} from "../../../utils";
 import Emergency from "../../../../common/Emergency";
-import JsonApiDocumentBuilder from "../../../jsonapi/JsonApiBuilder";
+import {default as JsonApiDocumentBuilder , DataType} from "../../../jsonapi/JsonApiBuilder";
+import {JsonApiDocument, JsonApiResource} from "../../../jsonapi/JsonApiObjects";
+import {RequirementType, ObjectParser} from "../../../jsonapi/Parser";
 
 export default class EmergencyResource extends ApiResource {
 
@@ -24,5 +27,33 @@ export default class EmergencyResource extends ApiResource {
         documentBuilder.setLinks(getLinkUrl(req, `/api/emergency`), null);
         documentBuilder.addResource(await this.serializerRegistry.serialize(emergency, req, documentBuilder));
         return documentBuilder.getProduct();
+    }
+
+    @ApiEndpoint('PATCH')
+    public async setEmergencyState (req: Request, h: ResponseToolkit) {
+        let parser = JsonApiDocument.getParser().addProperties({
+            name: 'data',
+            required: RequirementType.Required,
+            handler: JsonApiResource.getParser().addProperties({
+                name: 'attributes',
+                required: RequirementType.Required,
+                handler: new ObjectParser(() => ({}), {
+                    name: 'activate',
+                    required: RequirementType.Required
+                })
+            })
+        });
+
+        let activate: boolean;
+        try {
+            activate = (parser.parse(req.payload).data as JsonApiResource).attributes.activate;
+        } catch (err) {
+            winston.error(err);
+            return h.response({
+                error: 'Error while parsing the request. Argument might be missing.'
+            }).code(400);
+        }
+        await this.hedgehog.setEmergencyStop(activate);
+        return this.getEmergencyState(req);
     }
 }
