8403adaad67a2059cc4a052204775eaf75ebecad
Clemens Koza <koza@pria.at>
2019-09-20 18:04:12 +0200

    allow subscribing to either raw frames, or one of two channels (for faces and blobs)
    when implemented in the client, testing is needed for situations in which multiple browsers are open, opening and closing, and showing different channels.

--- src/server/hedgehog-io/SocketIoVisionAdapter.ts
+++ src/server/hedgehog-io/SocketIoVisionAdapter.ts
@@ -1,57 +1,87 @@
 import { HedgehogClient, Message, ack, vision } from 'hedgehog-client';
 
-export default class SocketIoVisionAdapter {
-    private ns: SocketIO.Namespace;
-    private requests: Message[];
-    private connectionCount: number = 0;
-    private timer?: any = null;
+class VisionNamespace {
+    public connectionCount: number = 0;
 
-    public constructor(private hedgehog: HedgehogClient, io: SocketIO.Server) {
-        console.log("setup vision");
-        this.ns = io.of('/vision');
-        this.requests = [
-            new vision.CaptureFrameAction(),
-            new vision.FrameRequest(null),
-        ];
-
+    public constructor(
+        public ns: SocketIO.Namespace,
+        onConnect: () => void,
+        onDisconnect: () => void,
+    ) {
         this.ns.on('connection', socket => {
-            if (this.connectionCount === 0) {
-                this.startUpdates();
-            }
+            onConnect();
             this.connectionCount += 1;
 
             socket.on('disconnect', () => {
                 this.connectionCount -= 1;
-                if (this.connectionCount === 0) {
-                    this.stopUpdates();
-                }
+                onDisconnect();
             });
         });
+    }
 
+    public async sendUpdate(hedgehog: HedgehogClient, highlight?: string) {
+        if (this.connectionCount === 0)
+            return;
+
+        let frame = await hedgehog.getFrame(highlight);
+        this.ns.emit('data', Buffer.from(frame.buffer, frame.byteOffset, frame.byteLength));
+    }
+}
+
+export default class SocketIoVisionAdapter {
+    private vision: VisionNamespace;
+    private faces: VisionNamespace;
+    private blobs: VisionNamespace;
+    private timer?: any = null;
+
+    public constructor(private hedgehog: HedgehogClient, io: SocketIO.Server) {
+        console.log("setup vision");
+        let onConnect = () => this.startUpdates();
+        let onDisconnect = () => this.stopUpdates();
+        this.vision = new VisionNamespace(io.of('/vision'), onConnect, onDisconnect);
+        this.faces = new VisionNamespace(io.of('/vision-faces'), onConnect, onDisconnect);
+        this.blobs = new VisionNamespace(io.of('/vision-blobs'), onConnect, onDisconnect);
     }
 
     private async startUpdates() {
-        console.log("subscribe to vision");
-        await this.hedgehog.openCamera();
-        this.timer = setInterval(() => this.sendVisionUpdate(), 500);
+        if (
+            this.vision.connectionCount === 0 &&
+            this.faces.connectionCount === 0 &&
+            this.blobs.connectionCount === 0
+        ) {
+            console.log("subscribe to vision");
+            await this.hedgehog.openCamera();
+            await this.hedgehog.createChannel('ide.faces', {
+                kind: vision.ChannelKind.FACES,
+            });
+            await this.hedgehog.createChannel('ide.blobs', {
+                kind: vision.ChannelKind.BLOBS,
+                hsvMin: 0x461414,
+                hsvMax: 0x5AFFEA,
+            });
+            this.timer = setInterval(() => this.sendVisionUpdate(), 500);
+        }
     }
 
     private async stopUpdates() {
-        clearInterval(this.timer);
-        this.timer = null;
-        await this.hedgehog.closeCamera();
-        console.log("unsubscribe from vision");
+        if (
+            this.vision.connectionCount === 0 &&
+            this.faces.connectionCount === 0 &&
+            this.blobs.connectionCount === 0
+        ) {
+            clearInterval(this.timer);
+            this.timer = null;
+            await this.hedgehog.deleteChannel('ide.faces');
+            await this.hedgehog.deleteChannel('ide.blobs');
+            await this.hedgehog.closeCamera();
+            console.log("unsubscribe from vision");
+        }
     }
 
     private async sendVisionUpdate() {
-        let [captureReply, frameReply] = await this.hedgehog.sendMultipart(...this.requests);
-        if (captureReply.code !== ack.AcknowledgementCode.OK)
-            return;
-
-        if (!(frameReply instanceof vision.FrameReply))
-            return;
-
-        let frame = frameReply.frame;
-        this.ns.emit('data', Buffer.from(frame.buffer, frame.byteOffset, frame.byteLength));
+        await this.hedgehog.captureFrame();
+        await this.vision.sendUpdate(this.hedgehog);
+        await this.faces.sendUpdate(this.hedgehog, 'ide.faces');
+        await this.blobs.sendUpdate(this.hedgehog, 'ide.faces');
     }
 }
