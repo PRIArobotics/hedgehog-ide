644d21f563a3f97bc36dd7bfa4c3ea3834a4f986
Clemens Koza <koza@pria.at>
2019-09-24 15:49:50 +0200

    add canvas that shows the color range

--- src/client/app/hedgehog-control/vision-control.component.ts
+++ src/client/app/hedgehog-control/vision-control.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {SafeUrl} from "@angular/platform-browser";
 
 import {VisionChannelKind} from "./http-hedgehog-client.service";
@@ -21,9 +21,15 @@ export default class VisionControlComponent {
     @Output() private channelChanged = new EventEmitter();
     @Output() private blobsRangeChanged = new EventEmitter();
 
+    @ViewChild('canvas') private canvas;
+
     private VisionChannelKind = VisionChannelKind;
 
     public constructor () {}
+
+    ngAfterViewInit() {
+        this.paintCanvas();
+    }
 
     private updateChannel (channel) {
         this.channel = channel;
@@ -90,11 +96,17 @@ export default class VisionControlComponent {
         return [r, g, b];
     }
 
-    public displayNumber(x: number): number {
+    public float2byte(x: number): number {
         return Math.round(x*255);
     }
 
-    private setBlobChannelRangeFromRgb(rgb: RGB) {
+    private setBlobsRange(range: HSVRange) {
+        this.blobsRange = range;
+        this.blobsRangeChanged.emit(range);
+        this.paintCanvas();
+    }
+
+    private setBlobsRangeFromRgb(rgb: RGB) {
         let [h, s, v] = VisionControlComponent.rgb2hsv(rgb);
 
         const range = (value, min, max, down, up) => [Math.max(min, value + down), Math.min(max, value + up)];
@@ -106,8 +118,7 @@ export default class VisionControlComponent {
         if (hMin < 0) hMin += 1;
         if (hMax >= 1) hMax -= 1;
 
-        this.blobsRange = [[hMin, sMin, vMin], [hMax, sMax, vMax]];
-        this.blobsRangeChanged.emit(this.blobsRange);
+        this.setBlobsRange([[hMin, sMin, vMin], [hMax, sMax, vMax]]);
     }
 
     private clickImage (event) {
@@ -124,7 +135,51 @@ export default class VisionControlComponent {
         let rgba = canvas.getContext('2d').getImageData(x, y, 1, 1).data;
         let rgb: RGB = [rgba[0]/255, rgba[1]/255, rgba[2]/255];
 
-        console.log(x, y, rgb, VisionControlComponent.rgb2hsv(rgb));
-        this.setBlobChannelRangeFromRgb(rgb);
+        this.setBlobsRangeFromRgb(rgb);
+    }
+
+    private paintCanvas() {
+        let ctx = this.canvas.nativeElement.getContext('2d');
+
+        let [[hMin, sMin, vMin], [hMax, sMax, vMax]] = this.blobsRange;
+		let hBias = hMin < hMax? (hMin+hMax)/2 : (hMin+hMax+1)/2;
+
+        const h2x = h => ((h - hBias + 3/2) % 1)*360;
+        const s2y = s => s * 100;
+        const v2y = v => 199 - v * 100;
+
+        const x2h = x => (x/360 + hBias + 1/2) % 1;
+        const y2sv = y => y < 100 ? [y / 100, 1] : [1, (199 - y) / 100];
+
+		for(let x = 0; x < 360; x++) {
+			for(let y = 0; y < 200; y++) {
+			    let hsv = [x2h(x), ...y2sv(y)] as HSV;
+				let [r, g, b] = VisionControlComponent.hsv2rgb(hsv);
+				ctx.fillStyle = `rgb(${this.float2byte(r)}, ${this.float2byte(g)}, ${this.float2byte(b)})`;
+				ctx.fillRect(x, y, 1, 1);
+			}
+		}
+
+		ctx.lineWidth = 2;
+		const r = 8;
+
+		let x1, x2, y1, y2;
+		x1 = h2x(hMin);
+		x2 = h2x(hMax);
+
+		y1 = s2y(sMin);
+		y2 = s2y(sMax);
+
+		ctx.strokeStyle = 'gray';
+		ctx.strokeRect(x1, y1, x2-x1, y2-y1);
+		ctx.strokeRect(x1-r, y1-r, 2*r, 2*r);
+		ctx.strokeRect(x2-r, y2-r, 2*r, 2*r);
+
+		y1 = v2y(vMin);
+		y2 = v2y(vMax);
+		ctx.strokeStyle = 'lightgray';
+		ctx.strokeRect(x1, y1, x2-x1, y2-y1);
+		ctx.strokeRect(x2-r, y1-r, 2*r, 2*r);
+		ctx.strokeRect(x1-r, y2-r, 2*r, 2*r);
     }
 }
